30cc5556e65f1d28f38333695b42c03613b4ba42
Fina Silva-Santisteban <fina@ltse.com>
2020-11-20 13:07:47 -0800

    add code

--- fina/test/test_wolf_in_sheeps_clothing.py
+++ fina/test/test_wolf_in_sheeps_clothing.py
@@ -5,20 +5,65 @@ from fina.code_challenges.wolf_in_sheeps_clothing.wolf_in_sheeps_clothing import
 class MyTestWolfCase(unittest.TestCase):
 
     def test_method_exists(self):
-        self.assertTrue(warn_the_sheep())
+        sheep_wolf_queue = ['sheep']
+        self.assertFalse(warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue))
 
-    #TODO: We forgot to test the parameter! Let's check if warn_the_sheep() takes ina parameter
-    # And another test for checking if parameter is list.
+    def test_when_wolf_in_front_of_line(self):
+        expected_value = 'Pls go away'
+        sheep_wolf_queue = ['sheep', 'wolf']
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
 
-    def test_returns_pls_go_away_sentence(self):
-        sentence_to_test = 'Pls go away and stop eating my sheep'
-        #TODO: There's another requirement for this test: if the wolf is the closest animal to you, return ^
-        self.assertEqual(sentence_to_test, warn_the_sheep())
+        self.assertEqual(expected_value, actual_value)
 
-    def test_returns_warning_sentence(self):
-        sentence_to_test = 'Oi! Sheep number N! You are about to be eaten by a wolf!'
-        #TODO: There's another requirement for this test: if the wolf is NOT the closest animal to you, return ^
-        self.assertEqual(sentence_to_test, warn_the_sheep('something'))
+    def test_when_wolf_not_in_front_of_line(self):
+        expected_value = 'Oi! Sheep 2, wolf is behind you!'
+        sheep_wolf_queue = ['wolf', 'sheep', 'sheep']
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+    def test_when_wolf_in_the_middle(self):
+        sheep_wolf_queue = ['sheep', 'wolf', 'sheep']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!'
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+#todo: fill out following tests
+    def test_when_wolf_in_the_middle_two_wolfs(self):
+        sheep_wolf_queue = ['sheep', 'wolf', 'wolf', 'sheep']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!' #todo
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+    def test_when_wolf_alternating_two_wolfs(self):
+        sheep_wolf_queue = ['wolf', 'sheep', 'wolf', 'sheep']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!' #todo
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+    def test_no_wolf(self):
+        sheep_wolf_queue = ['sheep', 'sheep']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!' #todo
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+    def test_no_sheep_only_wolf(self):
+        sheep_wolf_queue = ['wolf']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!' #todo
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
+
+    def test_other_animal(self):
+        sheep_wolf_queue = ['lion']
+        expected_value = 'Oi! Sheep 1, wolf is behind you!' #todo
+        actual_value = warn_the_sheep(sheep_wolf_queue=sheep_wolf_queue)
+
+        self.assertEqual(expected_value,actual_value)
 
 
 if __name__ == '__main__':
